35efb9f7fe9c75b09b99d7ca0096419018f3a900
Tien Nhan <nhandzzcuteo@gmail.com>
2023-11-18 15:11:41 +0700

    nhan commit

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
 from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
+
+# from flask_sqlalchemy import SQLAlchemy
 import getdata
 
 app = Flask(__name__)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "sqlite:///mydatabase.db"  # Sử dụng SQLite làm cơ sở dữ liệu
-db = SQLAlchemy(app)
+# app.config[
+#     "SQLALCHEMY_DATABASE_URI"
+# ] = "sqlite:///mydatabase.db"  # Sử dụng SQLite làm cơ sở dữ liệu
+# db = SQLAlchemy(app)
 
 
-# Định nghĩa model cho bảng User
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=True, nullable=False)
+# # Định nghĩa model cho bảng User
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=True, nullable=False)
 
 
-# Định nghĩa model cho bảng Post
-class Rating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    user = db.relationship("User", backref=db.backref("posts", lazy=True))
+# # Định nghĩa model cho bảng Post
+# class Rating(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(120), nullable=False)
+#     content = db.Column(db.Text, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+#     user = db.relationship("User", backref=db.backref("posts", lazy=True))
 
 
-# Tạo tất cả các bảng
-db.create_all()
+# # Tạo tất cả các bảng
+# db.create_all()
 
 
 @app.route("/")
